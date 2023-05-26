7b9a1fb242701dcf582e9b221b4619cc48f435ce
Ailakks <hola@cortmnzz.com>
2023-05-27 00:51:25 +0200

    feat: folder list arrow

--- src/components/ui/list/folder/folder-list.jsx
+++ src/components/ui/list/folder/folder-list.jsx
@@ -1,13 +1,18 @@
 import style from "./folder-list.module.css";
 
+import {Fragment} from "react";
+
 export default function FolderList({ list }) {
     return (
         <div className={style.container}>
             {
                 list.map((value, key) =>
-                    <div key={key}>
-                        <p>{value.name}</p>
-                    </div>
+                    <Fragment key={key}>
+                        <div>
+                            <p>{value.name}</p>
+                        </div>
+                        { key !== list.length - 1 && <i className="fa-light fa-chevron-right" /> }
+                    </Fragment>
                 )
             }
         </div>
