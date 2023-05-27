961929114825af3a0e3e189bc30b7ea67c9f8bd4
Ailakks <hola@cortmnzz.com>
2023-05-27 17:21:35 +0200

    feat: tool list

--- src/components/ui/list/file/file/file.jsx
+++ src/components/ui/list/file/file/file.jsx
@@ -1,6 +1,61 @@
 import style from "./file.module.css";
 
-export default function File({ value }) {
+export default function File({value}) {
+    const tools = [
+        {
+            list: [
+                {
+                    id: 'open',
+                    name: 'Open'
+                },
+                {
+                    id: 'download',
+                    name: 'Download'
+                }
+            ]
+        },
+        {
+            list: [
+                {
+                    id: 'share',
+                    name: 'Share'
+                },
+                {
+                    id: 'send',
+                    name: 'Send'
+                }
+            ]
+        },
+        {
+            list: [
+                {
+                    id: 'pin',
+                    name: 'Pin'
+                },
+                {
+                    id: 'color',
+                    name: 'Color'
+                },
+            ]
+        },
+        {
+            list: [
+                {
+                    id: 'clone',
+                    name: 'Clone'
+                },
+                {
+                    id: 'rename',
+                    name: 'Rename'
+                },
+                {
+                    id: 'remove',
+                    name: 'Remove'
+                }
+            ]
+        }
+    ]
+
     return (
         <div className={style.container}>
             <p>{value.file}</p>
