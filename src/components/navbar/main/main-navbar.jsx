98ec495443018136947325e12cefb76a44bb298b
Ailakks <hola@cortmnzz.com>
2023-05-28 00:06:12 +0200

    fix: main navbar display

--- src/components/navbar/main/main-navbar.jsx
+++ src/components/navbar/main/main-navbar.jsx
@@ -2,18 +2,16 @@ import style from "./main-navbar.module.css";
 
 import NavbarContainer from "../container/navbar-container";
 import PagesList from "../../pages/list/pages-list";
+import AuthBanner from "../../auth/banner/auth-banner";
 
 export default function MainNavbar() {
     return (
         <NavbarContainer>
             <div className={style.container}>
-                <div>
-
+                <div className={style.content}>
+                    <PagesList inline />
                 </div>
-                <PagesList inline />
-                <div>
-
-                </div>
+                <AuthBanner />
             </div>
         </NavbarContainer>
     )
