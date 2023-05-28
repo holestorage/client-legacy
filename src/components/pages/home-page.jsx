201f9d91d1196a155f55e8588e761394a5644a2b
Ailakks <hola@cortmnzz.com>
2023-05-28 18:18:39 +0200

    feat: home page section

--- src/components/pages/home-page.jsx
+++ src/components/pages/home-page.jsx
@@ -15,25 +15,29 @@ export default function HomePage() {
                 <h1>Encrypt. Protect. Control.</h1>
                 <h3>Your files encrypted with a passkey stored only on your device for ultimate security and privacy.
                     Access and control your data like never before.</h3>
-                <MainButton text="Get started" />
+                <MainButton text="Get started"/>
             </FullBannerBox>
             <PageContainer>
                 <BannerBox>
                     <h2>Encrypt. Protect. Control.</h2>
-                    <h3>Your files encrypted with a passkey stored only on your device for ultimate security and privacy.
+                    <h3>Your files encrypted with a passkey stored only on your device for ultimate security and
+                        privacy.
                         Access and control your data like never before.</h3>
                 </BannerBox>
                 <InlineBoxGrid>
                     <MainBox>
-                        <h3>A passkey is a 256-bit password that is stored locally on your devices and can also be synchronized
+                        <h3>A passkey is a 256-bit password that is stored locally on your devices and can also be
+                            synchronized
                             with your Google or iCloud account.</h3>
                     </MainBox>
                     <MainBox>
-                        <h3>Your files are encrypted with the access key, which is encrypted with your passkey, so no one can
+                        <h3>Your files are encrypted with the access key, which is encrypted with your passkey, so no
+                            one can
                             access them without that key.</h3>
                     </MainBox>
                     <MainBox>
-                        <h3>Neither attackers, nor governments, nor even ourselves will be able to access your files. Nobody.
+                        <h3>Neither attackers, nor governments, nor even ourselves will be able to access your files.
+                            Nobody.
                             What's yours is only yours.</h3>
                     </MainBox>
                 </InlineBoxGrid>
@@ -42,22 +46,55 @@ export default function HomePage() {
                 <InlineBoxGrid>
                     <BannerBox>
                         <h2>Unleash the Speed of Distributed Storage.</h2>
-                        <h3>Files are fragmented into small fractions and stored and distributed with redundancy among hundreds of servers around the world.</h3>
+                        <h3>Files are fragmented into small fractions and stored and distributed with redundancy among
+                            hundreds of servers around the world.</h3>
                     </BannerBox>
                     <BlockBoxGrid>
                         <MainBox>
                             <h3>Durability</h3>
-                            <h3>Infinitely reduce the risk of data loss compared to any other alternative, as files are replicated multiple times in different parts of the world.</h3>
+                            <h3>Infinitely reduce the risk of data loss compared to any other alternative, as files are
+                                replicated multiple times in different parts of the world.</h3>
                         </MainBox>
                         <MainBox>
                             <h3>Speed</h3>
-                            <h3>File access is always performed through the node closest to the user, allowing infinitely faster access to files from anywhere in the world.</h3>
+                            <h3>File access is always performed through the node closest to the user, allowing
+                                infinitely faster access to files from anywhere in the world.</h3>
                         </MainBox>
                         <MainBox>
                             <h3>Replication</h3>
-                            <h3>In the event that a node loses the stored files, they are instantly replicated again to ensure a minimum number of replicas.</h3>
+                            <h3>In the event that a node loses the stored files, they are instantly replicated again to
+                                ensure a minimum number of replicas.</h3>
                         </MainBox>
                     </BlockBoxGrid>
+                </InlineBoxGrid>
+            </PageContainer>
+            <PageContainer>
+                <InlineBoxGrid>
+                    <InlineBoxGrid>
+                        <BannerBox>
+                            <h2>The power is yours.</h2>
+                            <h3>Unlock all potential and play with your files.</h3>
+                        </BannerBox>
+                        <MainBox>
+                            <h3>Your files, secure and accessible anywhere.</h3>
+                            <h3>Avoid troubles with your hard drives and secure your belongings in data centers
+                                monitored day and night. Access them from anywhere with a touch, thanks to the passkey
+                                system.</h3>
+                        </MainBox>
+                        <MainBox>
+                            <h3>Share encrypted files.</h3>
+                            <h3>When sharing files with other users, they will be encrypted with their public keys, and
+                                only they, with their private key, will be able to unlock them, preventing anyone from
+                                intercepting the process.
+                            </h3>
+                        </MainBox>
+                        <MainBox>
+                            <h3>Send and share with the world.</h3>
+                            <h3>You can generate links for your files to share or use them, and we will keep an
+                                unencrypted copy for that purpose. If you assign an expiration period or manually expire
+                                them, we will destroy the unencrypted copy.</h3>
+                        </MainBox>
+                    </InlineBoxGrid>
                 </InlineBoxGrid>
             </PageContainer>
         </Fragment>
--- src/components/ui/container/page/page-container.jsx
+++ src/components/ui/container/page/page-container.jsx
@@ -1,8 +1,8 @@
 import style from "./page-container.module.css";
 
-export default function PageContainer({ children }) {
+export default function PageContainer({ color, children }) {
     return (
-        <div className={style.container}>
+        <div className={style.container} style={{ background: color }}>
             <div className={style.content}>
                 {children}
             </div>
