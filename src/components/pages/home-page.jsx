86b5b51b997a7669ef0d1759161aef7e0dc1ae1d
Ailakks <hola@cortmnzz.com>
2023-05-28 14:41:04 +0200

    feat: home page font style

--- src/components/pages/home-page.jsx
+++ src/components/pages/home-page.jsx
@@ -2,6 +2,7 @@ import PageContainer from "../ui/container/page/page-container";
 import BannerBox from "../ui/box/banner/banner-box";
 import MainBox from "../ui/box/main/main-box";
 import InlineBoxGrid from "../ui/box/grid/inline-box-grid";
+import MainButton from "../ui/button/main/main-button";
 
 export default function HomePage() {
     return (
@@ -10,19 +11,20 @@ export default function HomePage() {
                 <h1>Encrypt. Protect. Control.</h1>
                 <h3>Your files encrypted with a passkey stored only on your device for ultimate security and privacy.
                     Access and control your data like never before.</h3>
+                <MainButton text="Get started" />
             </BannerBox>
             <InlineBoxGrid>
                 <MainBox>
-                    <p>A passkey is a 256-bit password that is stored locally on your devices and can also be synchronized
-                        with your Google or iCloud account.</p>
+                    <h3>A passkey is a 256-bit password that is stored locally on your devices and can also be synchronized
+                        with your Google or iCloud account.</h3>
                 </MainBox>
                 <MainBox>
-                    <p>Your files are encrypted with the access key, which is encrypted with your passkey, so no one can
-                        access them without that key.</p>
+                    <h3>Your files are encrypted with the access key, which is encrypted with your passkey, so no one can
+                        access them without that key.</h3>
                 </MainBox>
                 <MainBox>
-                    <p>Neither attackers, nor governments, nor even ourselves will be able to access your files. Nobody.
-                        What's yours is only yours.</p>
+                    <h3>Neither attackers, nor governments, nor even ourselves will be able to access your files. Nobody.
+                        What's yours is only yours.</h3>
                 </MainBox>
             </InlineBoxGrid>
         </PageContainer>
