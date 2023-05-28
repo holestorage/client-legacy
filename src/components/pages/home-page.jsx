1c1128e2fc1faa43d27110f293d7c62d5d288cdc
Ailakks <hola@cortmnzz.com>
2023-05-28 14:44:51 +0200

    feat: page container full style

--- src/components/pages/home-page.jsx
+++ src/components/pages/home-page.jsx
@@ -1,3 +1,5 @@
+import {Fragment} from "react";
+
 import PageContainer from "../ui/container/page/page-container";
 import BannerBox from "../ui/box/banner/banner-box";
 import MainBox from "../ui/box/main/main-box";
@@ -6,27 +8,29 @@ import MainButton from "../ui/button/main/main-button";
 
 export default function HomePage() {
     return (
-        <PageContainer>
-            <BannerBox>
-                <h1>Encrypt. Protect. Control.</h1>
-                <h3>Your files encrypted with a passkey stored only on your device for ultimate security and privacy.
-                    Access and control your data like never before.</h3>
-                <MainButton text="Get started" />
-            </BannerBox>
-            <InlineBoxGrid>
-                <MainBox>
-                    <h3>A passkey is a 256-bit password that is stored locally on your devices and can also be synchronized
-                        with your Google or iCloud account.</h3>
-                </MainBox>
-                <MainBox>
-                    <h3>Your files are encrypted with the access key, which is encrypted with your passkey, so no one can
-                        access them without that key.</h3>
-                </MainBox>
-                <MainBox>
-                    <h3>Neither attackers, nor governments, nor even ourselves will be able to access your files. Nobody.
-                        What's yours is only yours.</h3>
-                </MainBox>
-            </InlineBoxGrid>
-        </PageContainer>
+        <Fragment>
+            <PageContainer>
+                <BannerBox>
+                    <h1>Encrypt. Protect. Control.</h1>
+                    <h3>Your files encrypted with a passkey stored only on your device for ultimate security and privacy.
+                        Access and control your data like never before.</h3>
+                    <MainButton text="Get started" />
+                </BannerBox>
+                <InlineBoxGrid>
+                    <MainBox>
+                        <h3>A passkey is a 256-bit password that is stored locally on your devices and can also be synchronized
+                            with your Google or iCloud account.</h3>
+                    </MainBox>
+                    <MainBox>
+                        <h3>Your files are encrypted with the access key, which is encrypted with your passkey, so no one can
+                            access them without that key.</h3>
+                    </MainBox>
+                    <MainBox>
+                        <h3>Neither attackers, nor governments, nor even ourselves will be able to access your files. Nobody.
+                            What's yours is only yours.</h3>
+                    </MainBox>
+                </InlineBoxGrid>
+            </PageContainer>
+        </Fragment>
     )
 }
--- src/components/ui/container/page/page-container.jsx
+++ src/components/ui/container/page/page-container.jsx
@@ -3,7 +3,9 @@ import style from "./page-container.module.css";
 export default function PageContainer({ children }) {
     return (
         <div className={style.container}>
-            {children}
+            <div className={style.content}>
+                {children}
+            </div>
         </div>
     )
 }
