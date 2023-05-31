453a08998ac3e1eb34f266b6fcf418e3c74fb664
Ailakks <hola@cortmnzz.com>
2023-05-31 19:37:56 +0200

    feat: header line

--- src/components/ui/header/main/header-main.jsx
+++ src/components/ui/header/main/header-main.jsx
@@ -3,12 +3,15 @@ import style from "./header-main.module.css";
 export default function HeaderMain({ children, left }) {
     return (
         <div className={style.container}>
-            <div>
-                {children}
+            <div className={style.content}>
+                <div>
+                    {children}
+                </div>
+                <div>
+                    {left}
+                </div>
             </div>
-            <div>
-                {left}
-            </div>
+            <hr />
         </div>
     )
 }
