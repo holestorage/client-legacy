54bcf885fbdb7f444d03b627119e16efaade64de
Ailakks <hola@cortmnzz.com>
2023-06-24 18:08:39 +0200

    fix: per icon load status

--- src/components/ui/button/icon/icon-button.jsx
+++ src/components/ui/button/icon/icon-button.jsx
@@ -1,8 +1,19 @@
 import style from "./icon-icon.module.css";
 
+import {useState} from "react";
+
 export default function IconButton({ icon, text, action }) {
+    const [loading, setLoading] = useState(false);
+
+    if (loading) {
+        return <p>...</p>
+    }
+
     return (
-        <button onClick={action} className={style.button}>
+        <button onClick={() => {
+            setLoading(true);
+            action().then(() => setLoading(false));
+        }} className={style.button}>
             <i className={icon} />
             <p>{text}</p>
         </button>
--- src/components/ui/list/tool/tool-list.jsx
+++ src/components/ui/list/tool/tool-list.jsx
@@ -1,14 +1,12 @@
 import style from "./tool-list.module.css";
 
-import {Fragment, useContext, useState} from "react";
+import {Fragment, useContext} from "react";
 
 import IconButton from "../../button/icon/icon-button";
 import PopupDialog from "../../../popup/dialog/popup-dialog";
 import {PopupContext} from "../../../provider/popup-provider";
 
 export default function ToolList({ data, section }) {
-    const [loading, setLoading] = useState(false);
-
     const popupContext = useContext(PopupContext);
 
     return (
@@ -19,12 +17,9 @@ export default function ToolList({ data, section }) {
                         {
                             value.list.map((value, key) =>
                                 <Fragment key={key}>
-                                    {loading ? <></> : <IconButton icon={value.icon} action={value.dialog ? () => popupContext.setCurrent(
+                                    <IconButton icon={value.icon} action={() => value.dialog ? popupContext.setCurrent(
                                         <PopupDialog title={value.dialog.title} body={value.dialog.body}
-                                                     button={{ text: value.dialog.button.text, type: value.dialog.button.style, action: () => value.action(data) }} />) : () => {
-                                        setLoading(true);
-                                        value.action(data).then(() => setLoading(false));
-                                    }} />}
+                                                     button={{ text: value.dialog.button.text, type: value.dialog.button.style, action: () => value.action(data) }} />) : value.action(data)}/>
                                 </Fragment>
                             )
                         }
