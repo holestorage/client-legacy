23c3cee76dd91b2237e9061ab0122435ce06d645
Ailakks <hola@cortmnzz.com>
2023-06-25 19:56:04 +0200

    feat: category content

--- src/components/ui/list/folder/content/category/category-content.jsx
+++ src/components/ui/list/folder/content/category/category-content.jsx
@@ -1,39 +1,8 @@
-import {Fragment, useEffect, useState} from "react";
-import HeaderMain from "../../../../header/main/header-main";
-import MainButton from "../../../../button/main/main-button";
-import PathDisplay from "../../../path/display/path-display";
-import {HoleApi} from "../../../../../../App";
 import {useParams} from "react-router-dom";
-import Container from "../../../../container/container";
 import SectionContent from "../section/section-content";
 
-export default function CategoryContent({ section, fallback }) {
-    const [data, setData] = useState(null);
-
-    const { id } = useParams();
-
-    const fetchContent = () => {
-        HoleApi.get(`files/${section}`).then(response => setData(response.data));
-    };
-
-    useEffect(() => fetchContent(), [id]);
-
-    if (data) {
-        const files = data.folder.files;
-        const folders = data.folder.folders;
-
-        return (
-            <Container>
-                <HeaderMain left={
-                    <Fragment>
-                        <MainButton icon="fa-regular fa-angle-up" text="Upload file" />
-                        <MainButton icon="fa-regular fa-folder" text="Create folder" />
-                    </Fragment>
-                }>
-                    <PathDisplay data={data.folder} />
-                </HeaderMain>
-                <SectionContent files={files} folders={folders} fallback={fallback} />
-            </Container>
-        )
-    }
+export default function CategoryContent({ category, fallback }) {
+    return (
+        <SectionContent path={`files/${category}`} fallback={fallback} />
+    )
 }
--- src/components/ui/list/folder/content/section/section-content.jsx
+++ src/components/ui/list/folder/content/section/section-content.jsx
@@ -1,12 +1,41 @@
-import {Fragment} from "react";
+import {Fragment, useEffect, useState} from "react";
+import HeaderMain from "../../../../header/main/header-main";
+import MainButton from "../../../../button/main/main-button";
+import PathDisplay from "../../../path/display/path-display";
 import FileList from "../../../file/file-list";
 import FolderList from "../../folder-list";
+import {HoleApi} from "../../../../../../App";
+import {useParams} from "react-router-dom";
+import Container from "../../../../container/container";
 
-export default function SectionContent({ files, folders, fallback }) {
-    return (
-        <Fragment>
-            {folders.length > 0 && <FolderList list={folders} />}
-            {files.length > 0 ? <FileList list={files} /> : fallback}
-        </Fragment>
-    )
+export default function SectionContent({ path, fallback }) {
+    const [data, setData] = useState(null);
+
+    const { id } = useParams();
+
+    const fetchContent = () => {
+        HoleApi.get(path).then(response => setData(response.data));
+    };
+
+    useEffect(() => fetchContent(), [id]);
+
+    if (data) {
+        const files = data.folder.files;
+        const folders = data.folder.folders;
+
+        return (
+            <Container>
+                <HeaderMain left={
+                    <Fragment>
+                        <MainButton icon="fa-regular fa-angle-up" text="Upload file" />
+                        <MainButton icon="fa-regular fa-folder" text="Create folder" />
+                    </Fragment>
+                }>
+                    <PathDisplay data={data.folder} />
+                </HeaderMain>
+                {folders.length > 0 && <FolderList list={folders} />}
+                {files.length > 0 ? <FileList list={files} /> : fallback}
+            </Container>
+        )
+    }
 }
