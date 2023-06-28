031b262118bf8e266223a5123bd1a4a168c6438c
Ailakks <hola@cortmnzz.com>
2023-06-28 22:03:18 +0200

    feat: file popup exit button

--- src/components/popup/container/popup-container.jsx
+++ src/components/popup/container/popup-container.jsx
@@ -1,12 +1,12 @@
 import style from "./popup-container.module.css";
 
-export default function PopupContainer({ close, children }) {
+export default function PopupContainer({ children }) {
     return (
         <div className={style.container}>
             <div className={style.content}>
                 {children}
             </div>
-            <div className={style.full} onClick={close} />
+            <div className={style.full} />
         </div>
     )
 }
--- src/components/popup/content/file/file-popup.jsx
+++ src/components/popup/content/file/file-popup.jsx
@@ -4,16 +4,18 @@ import {useNavigate} from "react-router-dom";
 import PopupContainer from "../../container/popup-container";
 import FilePreview from "../../../ui/list/file/file/preview/file-preview";
 import ToolList from "../../../ui/list/tool/tool-list";
+import IconButton from "../../../ui/button/icon/icon-button";
 
 export default function FilePopup({ folder, data }) {
     const navigate = useNavigate();
 
     return (
-        <PopupContainer close={() => navigate(`/folder/${folder}`)}>
+        <PopupContainer>
             <div className={style.container}>
                 <div className={style.header}>
                     <p>{data.name}</p>
                     <ToolList accessible data={data} filter={item => item.full} />
+                    <IconButton icon="fa-light fa-xmark" action={() => navigate(`/folder/${folder}`)} />
                 </div>
                 <div className={style.content}>
                     <FilePreview data={data} />
