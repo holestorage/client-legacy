7edecd4d058aa6146d681bcdc3f6dfaa213eef26
Ailakks <hola@cortmnzz.com>
2023-06-28 21:44:31 +0200

    feat: file popup container

--- src/components/popup/content/file/file-popup.jsx
+++ src/components/popup/content/file/file-popup.jsx
@@ -1,3 +1,5 @@
+import style from "./file-popup.module.css";
+
 import {useNavigate} from "react-router-dom";
 import PopupContainer from "../../container/popup-container";
 import FilePreview from "../../../ui/list/file/file/preview/file-preview";
@@ -8,8 +10,15 @@ export default function FilePopup({ folder, data }) {
 
     return (
         <PopupContainer close={() => navigate(`/folder/${folder}`)}>
-            <ToolList accessible data={data} filter={item => item.full} />
-            <FilePreview data={data} />
+            <div className={style.container}>
+                <div className={style.header}>
+                    <p>{data.name}</p>
+                    <ToolList accessible data={data} filter={item => item.full} />
+                </div>
+                <div className={style.content}>
+                    <FilePreview data={data} />
+                </div>
+            </div>
         </PopupContainer>
     )
 }
