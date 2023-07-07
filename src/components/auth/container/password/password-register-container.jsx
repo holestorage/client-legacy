5b8cacdff9e9c08568ba99d4634e08f14dc0fec5
Ailakks <hola@cortmnzz.com>
2023-07-08 00:33:58 +0200

    feat: password register container

---
+++ src/components/auth/container/password/password-register-container.jsx
@@ -0,0 +1,33 @@
+import {useNavigate} from 'react-router-dom';
+import MainBox from "../../../ui/box/main/main-box";
+import MainButton from "../../../ui/button/main/main-button";
+import {cookies, HoleApi} from "../../../../App";
+import Form from "../../../../context/form";
+import Input from "../../../../context/input";
+
+export default function PasswordRegisterContainer() {
+    const navigate = useNavigate();
+
+    const login = async (data) => {
+        return await HoleApi.post('user', data)
+            .then((response) => response.data)
+            .then((data) => data.attempt.key)
+            .then((key) => {
+                HoleApi.post('user', {key: key, password: data.password})
+                    .then((response) => response.data)
+                    .then((data) => cookies.set('token', data.token))
+                    .then((navigate('/')));
+            })
+    };
+
+    return (
+        <MainBox>
+            <Form onSubmit={login}>
+                <Input name="name" type="text" placeholder="Name" />
+                <Input name="email" type="email" placeholder="Email" />
+                <Input name="key" type="password" placeholder="Password" />
+                <MainButton type="submit" text="Continue"/>
+            </Form>
+        </MainBox>
+    );
+}
--- src/components/view/app/auth-view.jsx
+++ src/components/view/app/auth-view.jsx
@@ -2,6 +2,7 @@ import {Route, Routes, useLocation} from "react-router-dom";
 
 import NotFoundFallback from "../../pages/fallback/not-found-fallback";
 import PasswordLoginContainer from "../../auth/container/password/password-login-container";
+import PasswordRegisterContainer from "../../auth/container/password/password-register-container";
 
 export default function AuthView() {
     const location = useLocation();
@@ -9,7 +10,7 @@ export default function AuthView() {
     return (
         <Routes location={location}>
             <Route path="/login" element={<PasswordLoginContainer />}/>
-            <Route path="/register" element={<PasswordLoginContainer />}/>
+            <Route path="/register" element={<PasswordRegisterContainer />}/>
 
             <Route path="/*" element={<NotFoundFallback />}/>
         </Routes>
