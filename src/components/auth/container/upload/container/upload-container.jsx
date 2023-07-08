983d8bf9c6ac8c30ddf53824c62dcb2d5f900fe0
Ailakks <hola@cortmnzz.com>
2023-07-08 02:38:06 +0200

    feat: upload container

---
+++ src/components/auth/container/upload/container/upload-container.jsx
@@ -0,0 +1,9 @@
+import style from "./upload-container.module.css";
+
+export default function UploadContainer() {
+    return (
+        <div className={style.container}>
+
+        </div>
+    );
+}
---
+++ src/components/auth/container/upload/track/upload-track.jsx
@@ -0,0 +1,9 @@
+import style from "./upload-track.module.css";
+
+export default function UploadTrack() {
+    return (
+        <div className={style.container}>
+
+        </div>
+    );
+}
